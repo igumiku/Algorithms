d641031a679c83c3ad5d89f32a64f9b8be2656fd
lijiang <lijiang@didichuxing.com>
2019-03-22 15:35:12 +0800

    init commit

---
+++ src/main/java/airbnb/DisplayPage.java
@@ -0,0 +1,13 @@
+package airbnb;
+
+/**
+ * 4 Display Page (Pagination)
+ * Given an array of CSV strings representing search results, output results sorted by a score initially. A given host may have several listings that show up in these results. Suppose we want to show 12 results per page, but we don’t want the same host to dominate the results.
+ * <p>
+ * Write a function that will reorder the list so that a host shows up at most once on a page if possible, but otherwise preserves the ordering. Your program should return the new array and print out the results in blocks representing the pages.
+ * <p>
+ * Given an array of csv strings, output results separated by a blank line.
+ */
+public class DisplayPage {
+
+}
